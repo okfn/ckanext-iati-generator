3f408e7fb579a126d51f90f66661fb452ae42170
german <germankay@gmail.com>
2025-05-09 13:54:45 -0300

    fixes

--- ckanext/iati_generator/blueprint/iati.py
+++ ckanext/iati_generator/blueprint/iati.py
@@ -1,11 +1,24 @@
 from flask import Blueprint, render_template
 from ckan.plugins import toolkit
+from ckanext.iati_generator.decorators import require_sysadmin_user
 
 iati_blueprint = Blueprint("iati_generator", __name__, url_prefix="/iati-dataset")
 
 
+@require_sysadmin_user
 @iati_blueprint.route("/<package_id>", methods=["GET"])
 def iati_page(package_id):
     context = {"user": toolkit.c.user}
     toolkit.check_access("sysadmin", context)
-    return render_template("package/iati_page.html", pkg_id=package_id)
+    # Fetch the package using package_show
+    try:
+        pkg_dict = toolkit.get_action("package_show")(context, {"id": package_id})
+    except toolkit.ObjectNotFound:
+        return toolkit.abort(404, toolkit._("Dataset not found"))
+
+    # Pass both pkg and pkg_dict to the template (CKAN templates use both)
+    return render_template(
+        "package/iati_page.html",
+        pkg=pkg_dict,
+        pkg_dict=pkg_dict
+    )
