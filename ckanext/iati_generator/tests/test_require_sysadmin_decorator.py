e6e322edfa79b41bda09aae8749567f6d314ed6a
german <germankay@gmail.com>
2025-05-15 16:21:55 -0300

    fix

--- ckanext/iati_generator/tests/test_require_sysadmin_decorator.py
+++ ckanext/iati_generator/tests/test_require_sysadmin_decorator.py
@@ -1,6 +1,7 @@
 from ckan.lib.helpers import url_for
 from ckan.tests import factories
 
+
 class TestIatiTab:
 
     def test_require_sysadmin_user_decorator_no_user(self, app):
@@ -9,11 +10,10 @@ class TestIatiTab:
         when no user is logged in.
         """
         org = factories.Organization()
-        dataset = factories.Dataset(private=True, owner_org=org["id"])
+        dataset = factories.Dataset(owner_org=org["id"])
         url = url_for('iati_generator.iati_page', package_id=dataset['id'])
         response = app.get(url, expect_errors=True)
         assert response.status_code == 403
-
 
     def test_require_sysadmin_user_decorator_non_admin(self, app):
         """
@@ -21,7 +21,8 @@ class TestIatiTab:
         when a non-admin user is logged in.
         """
         user = factories.UserWithToken()
-        dataset = factories.Dataset(private=True)
+        org = factories.Organization()
+        dataset = factories.Dataset(owner_org=org["id"])
         url = url_for('iati_generator.iati_page', package_id=dataset['id'])
         auth = {"Authorization": user['token']}
         response = app.get(url, headers=auth, expect_errors=True)
@@ -33,7 +34,8 @@ class TestIatiTab:
         when a sysadmin user is logged in.
         """
         user_sysadmin = factories.SysadminWithToken()
-        dataset = factories.Dataset(private=True)
+        org = factories.Organization()
+        dataset = factories.Dataset(owner_org=org["id"])
         url = url_for('iati_generator.iati_page', package_id=dataset['id'])
         auth = {"Authorization": user_sysadmin['token']}
         response = app.get(url, headers=auth)
