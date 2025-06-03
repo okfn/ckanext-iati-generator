fd4402855f1ff62f4c81a4d58f969042b48bf914
German Favaro <germankay@gmail.com>
2025-06-03 14:03:51 -0300

    add coment

--- ckanext/iati_generator/blueprint/iati.py
+++ ckanext/iati_generator/blueprint/iati.py
@@ -23,11 +23,13 @@ def index():
 @require_sysadmin_user
 def iati_page(package_id):
     context = {"user": toolkit.c.user}
+    # Fetch the package using package_show
     try:
         pkg_dict = toolkit.get_action("package_show")(context, {"id": package_id})
     except toolkit.ObjectNotFound:
         return toolkit.abort(404, toolkit._("Dataset not found"))
 
+    # Pass both pkg and pkg_dict to the template (CKAN templates use both)
     ctx = {
         "pkg": pkg_dict,
         "pkg_dict": pkg_dict,
