677c0913ebb9c971889ff2fd67aa1336f805f9f4
German Favaro <germankay@gmail.com>
2025-10-22 11:24:03 -0300

    desase fix

--- ckanext/iati_generator/tests/conftest.py
+++ ckanext/iati_generator/tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 
 @pytest.fixture
-def clean_db(reset_db, migrate_db_for, with_plugins):
+def clean_db(reset_db, migrate_db_for):
     """Clean and initialize the database."""
     reset_db()
     migrate_db_for("iati_generator")
