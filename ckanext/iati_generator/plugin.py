3305fa5ed46211e9fe523f7ece6217af5ba18755
German Favaro <germankay@gmail.com>
2025-10-31 11:58:11 -0300

    Add public IATI links functionality and update templates
    - Implemented a new public endpoint for serving IATI XML files.
    - Added logic to build public IATI links for datasets.
    - Updated the IATI page template to display public links with status indicators.

--- ckanext/iati_generator/plugin.py
+++ ckanext/iati_generator/plugin.py
@@ -9,6 +9,7 @@ from ckanext.iati_generator.auth import iati as iati_auth
 from ckan.lib.plugins import DefaultTranslation
 from ckanext.iati_generator.blueprint.iati import iati_blueprint
 from ckanext.iati_generator.blueprint.admin import iati_blueprint_admin, iati_file_admin
+from ckanext.iati_generator.blueprint.public_iati import public_iati_blueprint
 from ckanext.iati_generator import helpers as h
 
 
@@ -32,6 +33,7 @@ class IatiGeneratorPlugin(p.SingletonPlugin, DefaultTranslation):
         return [
             iati_blueprint,
             iati_blueprint_admin,
+            public_iati_blueprint,
             iati_file_admin,
         ]
 
