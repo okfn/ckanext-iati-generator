eaaa892b0f9a01eb6115ab9fb93f8fd912cbf277
German Favaro <germankay@gmail.com>
2025-11-25 16:21:10 -0300

    helper delete

--- ckanext/iati_generator/helpers.py
+++ ckanext/iati_generator/helpers.py
@@ -1,19 +1,10 @@
 import logging
-from ckan.plugins import toolkit
 from ckan import model
 from ckanext.iati_generator.models.enums import IATIFileTypes
 from ckanext.iati_generator.models.iati_files import DEFAULT_NAMESPACE, IATIFile
 
 
 log = logging.getLogger(__name__)
-
-
-def iati_tab_enabled():
-    val = toolkit.config.get("ckanext.iati_generator.hide_tab", "false")
-    if not val:
-        val = "false"
-    bool_val = toolkit.asbool(val)
-    return not bool_val
 
 
 def iati_file_types(field=None):
