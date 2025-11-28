05d2bb44194426f7c595121542edd09eda2888ec
German Favaro <germankay@gmail.com>
2025-11-28 12:03:21 -0300

    Fix namespace retrieval in IATI files index view

--- ckanext/iati_generator/blueprint/admin.py
+++ ckanext/iati_generator/blueprint/admin.py
@@ -56,7 +56,7 @@ def iati_files_index():
 
         # Append row
         rows_out.append({
-            "namespace": item.get("namespace", ""),
+            "namespace": iati_file.get("namespace", ""),
             "file_type": iati_file.get("file_type", ""),
             "resource_name": resource.get("name") or res_id,
             "resource_id": res_id,
