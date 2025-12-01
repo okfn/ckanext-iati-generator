584b3de84620a4ca8d83e57c51dd6e1df7aa00bb
German Favaro <germankay@gmail.com>
2025-12-01 11:46:27 -0300

    Refactor IATIFileTypes enum to uncomment and define organization and activity file types

--- ckanext/iati_generator/models/enums.py
+++ ckanext/iati_generator/models/enums.py
@@ -3,19 +3,25 @@ from enum import Enum
 
 class IATIFileTypes(Enum):
     """ IATI elements are contained in files for each type of data """
+    # ------------------------------------------------------------------
+    # ORGANISATION FILE TYPES
+    # ------------------------------------------------------------------
     ORGANIZATION_MAIN_FILE = 100
     ORGANIZATION_NAMES_FILE = 110
-    # Uncomment once needed
-    ORGANIZATION_BUDGET_FILE = 20
-    ORGANIZATION_EXPENDITURE_FILE = 30
-    ORGANIZATION_DOCUMENT_FILE = 40
-    # ACTIVITY_MAIN_FILE = 50
-    # ACTIVITY_BUDGET_FILE = 60
-    # ACTIVITY_EXPENDITURE_FILE = 70
-    # ACTIVITY_DOCUMENT_FILE = 80
-    # ACTIVITY_LOCATION_FILE = 90
-    # ACTIVITY_SECTOR_FILE = 100
-    # ACTIVITY_PARTNER_FILE = 110
-    # ACTIVITY_RESULT_FILE = 120
-    # ACTIVITY_TRANSACTION_FILE = 130
-    # ACTIVITY_POLICY_FILE = 140
+    ORGANIZATION_BUDGET_FILE = 120
+    ORGANIZATION_EXPENDITURE_FILE = 130
+    ORGANIZATION_DOCUMENT_FILE = 140
+
+    # ------------------------------------------------------------------
+    # ACTIVITY FILE TYPES
+    # ------------------------------------------------------------------
+    ACTIVITY_MAIN_FILE = 200
+    ACTIVITY_BUDGET_FILE = 210
+    ACTIVITY_EXPENDITURE_FILE = 220
+    ACTIVITY_DOCUMENT_FILE = 230
+    ACTIVITY_LOCATION_FILE = 240
+    ACTIVITY_SECTOR_FILE = 250
+    ACTIVITY_PARTNER_FILE = 260
+    ACTIVITY_RESULT_FILE = 270
+    ACTIVITY_TRANSACTION_FILE = 280
+    ACTIVITY_POLICY_FILE = 290
