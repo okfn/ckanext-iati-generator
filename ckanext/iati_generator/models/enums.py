36d25ef23dd48e928624754416fff799bd134cbe
German Favaro <germankay@gmail.com>
2025-12-02 10:17:24 -0300

    Refactor IATIFileTypes enum to include detailed activity file types and ensure consistency with organization file types

--- ckanext/iati_generator/models/enums.py
+++ ckanext/iati_generator/models/enums.py
@@ -11,19 +11,27 @@ class IATIFileTypes(Enum):
     ORGANIZATION_BUDGET_FILE = 120
     ORGANIZATION_EXPENDITURE_FILE = 130
     ORGANIZATION_DOCUMENT_FILE = 140
+    FINAL_ORGANIZATION_FILE = 199
 
     # ------------------------------------------------------------------
     # ACTIVITY FILE TYPES
     # Always sync IatiMultiCsvConverter.csv_files
     # https://github.com/okfn/okfn_iati/blob/main/src/okfn_iati/multi_csv_converter.py#L56
     # ------------------------------------------------------------------
-    ACTIVITY_MAIN_FILE = 200
-    ACTIVITY_BUDGET_FILE = 210
-    ACTIVITY_EXPENDITURE_FILE = 220
-    ACTIVITY_DOCUMENT_FILE = 230
-    ACTIVITY_LOCATION_FILE = 240
-    ACTIVITY_SECTOR_FILE = 250
-    ACTIVITY_PARTNER_FILE = 260
-    ACTIVITY_RESULT_FILE = 270
-    ACTIVITY_TRANSACTION_FILE = 280
-    ACTIVITY_POLICY_FILE = 290
+    ACTIVITY_MAIN_FILE = 200                     # activities.csv
+    ACTIVITY_PARTICIPATING_ORGS_FILE = 210       # participating_orgs.csv
+    ACTIVITY_SECTORS_FILE = 220                  # sectors.csv
+    ACTIVITY_BUDGET_FILE = 230                   # budgets.csv
+    ACTIVITY_TRANSACTIONS_FILE = 240             # transactions.csv
+    ACTIVITY_TRANSACTION_SECTORS_FILE = 250      # transaction_sectors.csv
+    ACTIVITY_LOCATIONS_FILE = 260                # locations.csv
+    ACTIVITY_DOCUMENTS_FILE = 270                # documents.csv
+    ACTIVITY_RESULTS_FILE = 280                  # results.csv
+    ACTIVITY_INDICATORS_FILE = 290               # indicators.csv
+    ACTIVITY_INDICATOR_PERIODS_FILE = 300        # indicator_periods.csv
+    ACTIVITY_DATES_FILE = 310                    # activity_date.csv
+    ACTIVITY_CONTACT_INFO_FILE = 320             # contact_info.csv
+    ACTIVITY_CONDITIONS_FILE = 330               # conditions.csv
+    ACTIVITY_DESCRIPTIONS_FILE = 340             # descriptions.csv
+    ACTIVITY_COUNTRY_BUDGET_ITEMS_FILE = 350     # country_budget_items.csv
+    FINAL_ACTIVITY_FILE = 299
