b29570e11ca9b28a469b2d6d8602d1d29d101a77
German Favaro <germankay@gmail.com>
2026-01-02 12:20:52 -0300

    Refactor: Remove unused IATI dataset listing function and clean up admin routes

--- ckanext/iati_generator/blueprint/admin.py
+++ ckanext/iati_generator/blueprint/admin.py
@@ -3,18 +3,9 @@ from flask import Blueprint, request
 from ckan.plugins import toolkit
 
 from ckanext.iati_generator.decorators import require_sysadmin_user
-from ckanext.iati_generator.actions.iati import list_datasets_with_iati
 
-iati_blueprint_admin = Blueprint("iati_generator_admin", __name__, url_prefix="/ckan-admin/iati")
+
 iati_file_admin = Blueprint("iati_generator_admin_files", __name__, url_prefix="/ckan-admin/list-iati-files")
-
-
-@iati_blueprint_admin.route("/", methods=["GET"])
-@require_sysadmin_user
-def index():
-    context = {"user": toolkit.c.user}
-    datasets = list_datasets_with_iati(context)
-    return toolkit.render("iati/admin.html", {"datasets": datasets})
 
 
 @iati_file_admin.route("/iati-files")
