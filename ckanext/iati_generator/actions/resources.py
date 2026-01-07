7ca27530bfeceedce3881e3fb3787f8ee7f3af63
Patricio Del Boca <patriciodelboca@gmail.com>
2026-01-07 11:55:22 +0100

    Refactor resource chain actions for new schema.

--- ckanext/iati_generator/actions/resources.py
+++ ckanext/iati_generator/actions/resources.py
@@ -2,6 +2,8 @@
 Override the resources_create and resources_update actions to
 handle IATI file extras.
 """
+from os import name
+from ckan.tests.lib.navl.test_dictization_functions import data
 
 import logging
 
@@ -9,7 +11,7 @@ from ckan.plugins import toolkit
 from ckan import model
 
 from ckanext.iati_generator import helpers as h
-from ckanext.iati_generator.models.iati_files import IATIFile
+from ckanext.iati_generator.models.iati_files import IATIFile, DEFAULT_NAMESPACE
 
 log = logging.getLogger(__name__)
 
@@ -68,17 +70,55 @@ def _sync_iati_file_for_resource(context, resource_dict):
         )
 
 
+def iati_resource_create(context, data_dict):
+    """Creates an IATI File upon resource creation."""
+    iati_file_type = data_dict.get("iati_file_type", None)
+    if not iati_file_type:
+        return
+    pkg_dict = toolkit.get_action("package_show")({}, {"id": data_dict["package_id"]})
+    namespace = pkg_dict.get("iati_namespace")
+    if not namespace:
+        namespace = DEFAULT_NAMESPACE
+
+    toolkit.get_action('iati_file_create')({}, {
+        "resource_id": data_dict["id"],
+        "namespace": namespace,
+        "file_type": iati_file_type,
+    })
+
+
+def iati_resource_update(context, data_dict):
+    """Updates an IATI File upon resource update."""
+    res_id = data_dict['id']
+    existing = model.Session.query(IATIFile).filter_by(resource_id=res_id).first()
+
+    if not existing:
+        iati_resource_create(context, data_dict)
+        return
+
+    iati_file_type = data_dict.get("iati_file_type", None)
+    if not iati_file_type and existing:
+        toolkit.get_action("iati_file_delete")({},{"id": existing.id})
+        return
+
+    pkg_dict = toolkit.get_action("package_show")({}, {"id": data_dict["package_id"]})
+    namespace = pkg_dict.get("iati_namespace")
+    if not namespace:
+        namespace = DEFAULT_NAMESPACE
+
+    file_dict = { "id": existing.id, "file_type": iati_file_type, "namespace": namespace }
+    toolkit.get_action("iati_file_update")({},file_dict)
+
+
 @toolkit.chained_action
 def resource_create(up_function, context, data_dict):
-    # Call the core CKAN resource_create action
     resource = up_function(context, data_dict)
-    # Then synchronize the IATIFile
-    _sync_iati_file_for_resource(context, resource)
+    iati_resource_create(context, resource)
     return resource
 
 
 @toolkit.chained_action
 def resource_update(up_function, context, data_dict):
     resource = up_function(context, data_dict)
-    _sync_iati_file_for_resource(context, resource)
+    iati_resource_update(context, resource)
     return resource
