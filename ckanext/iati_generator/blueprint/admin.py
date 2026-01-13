60e45906bc30928b640f7fc1f01bfe7c597e3345
Patricio Del Boca <patriciodelboca@gmail.com>
2026-01-13 09:24:51 +0100

    Refactor view to use package_id as parameter

--- ckanext/iati_generator/blueprint/admin.py
+++ ckanext/iati_generator/blueprint/admin.py
@@ -10,42 +10,32 @@ iati_file_admin = Blueprint("iati_generator_admin_files", __name__, url_prefix="
 @iati_file_admin.route("/iati-files")
 @require_sysadmin_user
 def iati_files_index():
-    """List all IATI “files” (resources with IATI extras) in the system.
-    This is a simple admin view to see the status of IATI files.
-    """
-    namespace = request.args.get("namespace")
-    if not namespace:
-        return toolkit.render("iati/iati_files.html", {"namespace": ""})
+    """List IATI files and missing files for the selected dataset."""
+    search = toolkit.get_action('package_search')({}, {'fq': 'iati_namespace:[* TO *]'})
+    iati_datasets = [dataset for dataset in search.get("results", [])]
+    package_id = request.args.get("package_id")
+    if not package_id:
+        return toolkit.render("iati/iati_files.html", {"package_id": "", "iati_datasets": iati_datasets})
 
-    params = {}
-    params["namespace"] = namespace
-    search_filter = f"iati_namespace:{namespace}"
-    results = toolkit.get_action('package_search')({}, {'fq': search_filter})
+    dataset = toolkit.get_action('package_show')({}, {"id": package_id})
 
     rows_out = []
-    for dataset in results.get("results", []):
-        for res in dataset["resources"]:
-            # Call resource show to add IATI File fields to each resource dict.
-            resource = toolkit.get_action("resource_show")({}, {"id": res["id"]})
-            rows_out.append({
-                "namespace": dataset.get("iati_namespace"),
-                "file_type": resource.get("iati_file_type"),
-                "resource_name": resource.get("name") or resource.get("id"),
-                "resource_id": resource.get("id"),
-                "dataset_name": dataset.get("name"),
-                "valid": resource.get("iati_is_valid"),
-                "notes": "Notes from the backend...",
-                "resource_url": resource.get("url"),
-            })
+    for resource in dataset["resources"]:
+        url = toolkit.url_for("resource.read", package_type=dataset["type"], id=dataset["id"], resource_id=resource["id"])
+        rows_out.append({
+            "file_type": resource.get("iati_file_type"),
+            "resource_name": resource.get("name") or resource.get("id"),
+            "resource_url": url,
+        })
 
     pending_files = h.get_pending_mandatory_files(dataset["id"])
 
     return toolkit.render(
         "iati/iati_files.html",
         {
+            "iati_datasets": iati_datasets,
+            "package_id": package_id,
             "iati_files": rows_out,
-            "total": results.get("count", 0),
-            "namespace": namespace or "",
             "pending_files": pending_files,
         },
     )
