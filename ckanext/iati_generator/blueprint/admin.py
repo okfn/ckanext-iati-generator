b472a5d3320c83c5805275ca304a6ac54cf7cf3b
German Favaro <germankay@gmail.com>
2026-01-14 12:30:17 -0300

    Add endpoint to generate IATI organization XML file with error handling

--- ckanext/iati_generator/blueprint/admin.py
+++ ckanext/iati_generator/blueprint/admin.py
@@ -76,3 +76,26 @@ def generate_iati_activity_file(package_id):
 
     redirect_url = toolkit.url_for("iati_generator_admin_files.iati_files_index", package_id=package_id)
     return toolkit.redirect_to(redirect_url)
+
+
+@iati_file_admin.route("/generate-iati-organisation_file/<package_id>", methods=["POST"])
+@require_sysadmin_user
+def generate_iati_organisation_file(package_id):
+    ctx = {"user": toolkit.c.user}
+    data_dict = {"package_id": package_id}
+
+    toolkit.check_access("generate_organization_xml", ctx, data_dict)
+    try:
+        resource = toolkit.get_action("iati_generate_organisation_xml")(ctx, data_dict)
+        res_url = toolkit.url_for("resource.read", package_type="dataset", id=resource["package_id"], resource_id=resource["id"])
+        msg = toolkit._(f"IATI File generated successfully, you can <a href={res_url}>click here to go to the resource.</a>")
+        toolkit.h.flash_success(msg, allow_html=True)
+    except toolkit.ObjectNotFound:
+        toolkit.h.flash_error(toolkit._(f"The dataset with id {package_id} does not exist."))
+    except toolkit.ValidationError:
+        toolkit.h.flash_error(
+            toolkit._("There was an error generating the IATI file probably due to missing on wrong-formatted files.")
+        )
+
+    redirect_url = toolkit.url_for("iati_generator_admin_files.iati_files_index", package_id=package_id)
+    return toolkit.redirect_to(redirect_url)
