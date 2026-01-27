3b7fc3448fe23d7f0a3ab8b744fbfe878f980e8b
German Favaro <germankay@gmail.com>
2026-01-27 11:34:19 -0300

    Resolve final IATI XML resource reading iati_file_type from extras and public context

--- ckanext/iati_generator/blueprint/public_iati.py
+++ ckanext/iati_generator/blueprint/public_iati.py
@@ -13,6 +13,12 @@ def _find_final_resource(dataset: dict, file_type_value: int):
         ft = res.get("iati_file_type")
 
         if not ft:
+            for extra in res.get("extras", []):
+                if extra.get("key") == "iati_file_type":
+                    ft = extra.get("value")
+                    break
+
+        if not ft:
             continue
 
         if str(ft).isdigit() and int(ft) == file_type_value:
@@ -23,7 +29,7 @@ def _find_final_resource(dataset: dict, file_type_value: int):
 
 @iati_public.route("/<namespace>/organisation.xml")
 def public_org(namespace):
-    ctx = {"ignore_auth": True}
+    ctx = {"ignore_auth": True, "user": ""}
     dataset = toolkit.get_action("iati_get_dataset_by_namespace")(ctx, {"namespace": namespace})
 
     if not dataset:
@@ -38,7 +44,7 @@ def public_org(namespace):
 
 @iati_public.route("/<namespace>/activity.xml")
 def public_act(namespace):
-    ctx = {"ignore_auth": True}
+    ctx = {"ignore_auth": True, "user": ""}
     dataset = toolkit.get_action("iati_get_dataset_by_namespace")(ctx, {"namespace": namespace})
 
     if not dataset:
