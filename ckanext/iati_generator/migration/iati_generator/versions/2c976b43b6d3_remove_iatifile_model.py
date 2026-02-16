12a79f484561d0ff5f36b4a8cb17b948da0bc6ca
Patricio Del Boca <patriciodelboca@gmail.com>
2026-02-16 12:02:54 +0100

    Add migration to drop the IATI files table.

---
+++ ckanext/iati_generator/migration/iati_generator/versions/2c976b43b6d3_remove_iatifile_model.py
@@ -0,0 +1,24 @@
+"""Remove IATIFile model.
+
+Revision ID: 2c976b43b6d3
+Revises: 1245d2d05f24
+Create Date: 2026-02-16 10:54:29.611583
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2c976b43b6d3'
+down_revision = '1245d2d05f24'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_table('iati_files')
+
+
+def downgrade():
+    pass
